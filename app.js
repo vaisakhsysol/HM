b47f86bd9ae0aed2e3bde5091d463a7ad2f01b2d
vaisakhp98 <vaisakhp43@gmail.com>
2023-07-17 16:49:08 +0530

    Added search profile  section and fully Responsive

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ menuButton.addEventListener("click",()=>{
 document.querySelectorAll(".links").forEach(n=>n.addEventListener("click", ()=>{
     navLinks.classList.remove("active")
 }))
+
+
+linkHome = document.querySelector(".linkHome")
+linkAbout = document.querySelector(".linkAbout")
+linkDownload = document.querySelector(".linkDownload")
+linkContact = document.querySelector(".linkContact")
+
+linkHome.addEventListener("click",()=>{
+    linkHome.classList.toggle('active')
+})
+
+linkAbout.addEventListener("click",()=>{
+    linkAbout.classList.toggle('active')
+})
+
+linkDownload.addEventListener("click",()=>{
+    linkDownload.classList.toggle('active')
+})
+
+linkContact.addEventListener("click",()=>{
+    linkContact.classList.toggle('active')
+})
